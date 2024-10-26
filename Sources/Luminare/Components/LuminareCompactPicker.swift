a16c26a8382d3474484c06a87cf953c9c5f5358d
KrLite <wang070621@foxmail.com>
2024-10-26 21:27:13 +0800

    Compact picker - #4

--- Sources/Luminare/Components/LuminareCompactPicker.swift
+++ Sources/Luminare/Components/LuminareCompactPicker.swift
@@ -9,19 +9,13 @@ import SwiftUI
 
 struct LuminareCompactPicker<Content, V>: View
 where Content: View, V: Hashable & Equatable {
-    let cornerRadius: CGFloat
-    
     @Binding private var selection: V
     @ViewBuilder private let content: () -> Content
     
-    @State var isHovering: Bool = false
-    
     public init(
         selection: Binding<V>,
-        cornerRadius: CGFloat = 8,
         @ViewBuilder content: @escaping () -> Content
     ) {
-        self.cornerRadius = cornerRadius
         self._selection = selection
         self.content = content
     }
@@ -32,38 +26,26 @@ where Content: View, V: Hashable & Equatable {
         } label: {
             EmptyView()
         }
-        .padding(.leading, 2)
+        .padding(.trailing, -2)
         .buttonStyle(.borderless)
-        .fixedSize()
-        .background {
-            if isHovering {
-                Rectangle()
-                    .foregroundStyle(.quaternary.opacity(0.7))
-            } else {
-                Rectangle()
-                    .foregroundStyle(.quinary)
-            }
-        }
-        .background {
-            RoundedRectangle(cornerRadius: cornerRadius)
-                .strokeBorder(.quaternary, lineWidth: 1)
-        }
-        .clipShape(RoundedRectangle(cornerRadius: cornerRadius)).onHover { hover in
-            withAnimation(LuminareConstants.fastAnimation) {
-                isHovering = hover
-            }
-        }
-        .animation(LuminareConstants.fastAnimation, value: isHovering)
     }
 }
 
 #Preview {
     LuminareSection {
-        LuminareCompactPicker(selection: .constant(42)) {
-            ForEach(0..<200) { num in
-                Text("\(num)")
+        LuminareLabeledContent("Picker") {
+            Picker(selection: .constant(42)) {
+                ForEach(0..<200) { num in
+                    Text("\(num)")
+                }
+            } label: {
+                EmptyView()
             }
+            .frame(height: 30)
+            .padding(.horizontal, 8)
+            .buttonStyle(LuminareCompactButtonStyle(extraCompact: true))
         }
+        .padding(.trailing, -4)
         
         LuminareLabeledContent("Button") {
             Button {
