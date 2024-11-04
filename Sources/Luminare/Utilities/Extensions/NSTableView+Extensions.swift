69ca7ef588677a875ed09e9f5a1eeb3fd63709c2
KrLite <68179735+KrLite@users.noreply.github.com>
2024-11-04 11:35:12 +0800

    更新 NSTableView+Extensions.swift

--- Sources/Luminare/Utilities/Extensions/NSTableView+Extensions.swift
+++ Sources/Luminare/Utilities/Extensions/NSTableView+Extensions.swift
@@ -1,3 +1,12 @@
+//
+//  NSTableView+Extensions.swift
+//  
+//
+//  Created by KrLite on 2024/11/4.
+//
+
+import AppKit
+
 extension NSTableView {
     override open func viewDidMoveToWindow() {
         super.viewWillDraw()
