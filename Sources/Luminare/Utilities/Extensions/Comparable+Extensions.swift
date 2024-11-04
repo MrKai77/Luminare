6dab562dbba715a0463d35f64bdadfa68ec894fc
KrLite <68179735+KrLite@users.noreply.github.com>
2024-11-04 12:06:14 +0800

    更新 Comparable+Extensions.swift

--- Sources/Luminare/Utilities/Extensions/Comparable+Extensions.swift
+++ Sources/Luminare/Utilities/Extensions/Comparable+Extensions.swift
@@ -1,3 +1,12 @@
+//
+//  Comparable+Extensions.swift
+//  
+//
+//  Created by KrLite on 2024/11/3.
+//
+
+import SwiftUI
+
 extension Comparable {
     func clamped(to limits: ClosedRange<Self>) -> Self {
         min(max(self, limits.lowerBound), limits.upperBound)
