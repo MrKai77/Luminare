dd7d9b729ea7d9b56d628f7885de0c879871b35d
KrLite <wang070621@foxmail.com>
2024-11-24 16:09:17 +0800

    Fine. Remove SwiftLint.

--- Package.swift
+++ Package.swift
@@ -16,15 +16,13 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
-        .package(url: "https://github.com/realm/SwiftLint", branch: "main")
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0")
     ],
     targets: [
         // targets are the basic building blocks of a package, defining a module or a test suite
         // targets can depend on other targets in this package and products from dependencies
         .target(
-            name: "Luminare",
-            plugins: [.plugin(name: "SwiftLintBuildToolPlugin", package: "SwiftLint")]
+            name: "Luminare"
         ),
         .testTarget(
             name: "LuminareTests",
