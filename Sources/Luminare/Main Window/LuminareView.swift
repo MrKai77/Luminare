1bcf4492b7f92f0680f3239e6c69852eabe07bb4
Kai <hi@mrkai77.dev>
2025-03-09 20:03:04 -0600

    🚛 Move `LuminareView` window resize logic to `LuminareWindow`

--- Sources/Luminare/Main Window/LuminareView.swift
+++ Sources/Luminare/Main Window/LuminareView.swift
@@ -21,9 +21,11 @@ public struct LuminareView<Content>: View where Content: View {
     // MARK: Fields
 
     @ViewBuilder public let content: () -> Content
-
-    @State private var currentAnimation: LuminareWindowAnimation?
     @State private var contentSize: CGSize = .zero
+
+    public init(@ViewBuilder content: @escaping () -> Content) {
+        self.content = content
+    }
 
     // MARK: Body
 
@@ -32,96 +34,11 @@ public struct LuminareView<Content>: View where Content: View {
             .focusable(false)
             .buttonStyle(.luminare)
             .overrideTint(tint)
-            // Handle content size change
-            .onGeometryChange(for: CGSize.self) { proxy in
-                proxy.size
-            } action: { newValue in
-                contentSize = newValue
+            .onGeometryChange(for: CGSize.self, of: \.size) {
+                contentSize = $0
             }
-            .onAppear { setSize(size: contentSize, animate: false) }
-            .onChange(of: contentSize) { setSize(size: $0, animate: true) }
-    }
-
-    // MARK: Functions
-
-    func setSize(size: CGSize, animate: Bool) {
-        guard let window else { return }
-        guard size.width > 0, size.height > 0 else { return }
-
-        currentAnimation?.stop()
-
-        var frame = NSRect(
-            origin: window.frame.origin,
-            size: CGSize(
-                width: size.width,
-                height: size.height + 52 // 52 is the title bar height
-            )
-        )
-
-        if let screenFrame = window.screen?.visibleFrame {
-            if frame.minX < screenFrame.minX {
-                frame.origin.x = screenFrame.minX
-            }
-
-            if frame.minY < screenFrame.minY {
-                frame.origin.y = screenFrame.minY
-            }
-
-            if frame.maxX > screenFrame.maxX {
-                frame.origin.x = screenFrame.maxX - frame.width
-            }
-
-            if frame.maxY > screenFrame.maxY {
-                frame.origin.y = screenFrame.maxY - frame.height
-            }
-        }
-
-        if animate {
-            currentAnimation = LuminareWindowAnimation(window, frame)
-            currentAnimation?.start()
-        } else {
-            window.setFrame(frame, display: true)
-        }
-    }
-}
-
-// MARK: - NSWindow Animation
-
-// Vustom `NSWindow` resize animation so that it can be stopped midway
-class LuminareWindowAnimation: NSAnimation {
-    let window: NSWindow
-    let targetFrame: NSRect
-
-    init(_ window: NSWindow, _ targetFrame: NSRect) {
-        self.window = window
-        self.targetFrame = targetFrame
-        super.init(duration: 0.5, animationCurve: .easeOut)
-        super.animationBlockingMode = .nonblocking // allows the window to redraw contents while animating
-    }
-
-    @available(*, unavailable)
-    required init?(coder _: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
-    override var currentProgress: NSAnimation.Progress {
-        didSet {
-            // The last frame of this `NSAnimation` looks a little stuttery,
-            // so we multiply the progress by 1.01, and then make sure the last
-            // frame doesn't draw
-            let progress = CGFloat(currentProgress * 1.01)
-            guard progress < 1 else {
-                return
-            }
-
-            let currentFrame = NSRect(
-                x: window.frame.origin.x + (targetFrame.origin.x - window.frame.origin.x) * progress,
-                y: window.frame.origin.y + (targetFrame.origin.y - window.frame.origin.y) * progress,
-                width: window.frame.width + (targetFrame.width - window.frame.width) * progress,
-                height: window.frame.height + (targetFrame.height - window.frame.height) * progress
-            )
-
-            window.setFrame(currentFrame, display: false)
-        }
+            .onAppear { window?.setSize(size: contentSize, animate: false) }
+            .onChange(of: contentSize) { window?.setSize(size: $0, animate: true) }
+            .frame(minWidth: 10, maxWidth: .infinity, minHeight: 10, maxHeight: .infinity, alignment: .leading)
     }
 }
--- Sources/Luminare/Main Window/LuminareWindow.swift
+++ Sources/Luminare/Main Window/LuminareWindow.swift
@@ -9,15 +9,13 @@ import SwiftUI
 
 /// A stylized window with a materialized appearance.
 public class LuminareWindow: NSWindow {
-    private var initializationTime: Date
+    private var currentAnimation: LuminareWindowAnimation?
 
     /// Initializes a ``LuminareWindow``.
     ///
     /// - Parameters:
     ///   - content: the content view of the window, wrapped in a ``LuminareView``.
     public init(content: @escaping () -> some View) {
-        self.initializationTime = .now
-
         super.init(
             contentRect: .zero,
             styleMask: [.titled, .fullSizeContentView, .closable, .resizable],
@@ -35,18 +33,85 @@ public class LuminareWindow: NSWindow {
         titlebarAppearsTransparent = true
         titleVisibility = .hidden
         toolbar = NSToolbar()
-        alphaValue = 0
     }
 
-    /// Shows this window.
-    /// This action activates the current application and orders the window to the frontmost.
-    public func show() {
-        orderFrontRegardless()
-        NSApp.activate(ignoringOtherApps: true)
+    func setSize(size: CGSize, animate: Bool) {
+        guard size.width > 0, size.height > 0 else { return }
 
-        NSAnimationContext.runAnimationGroup { context in
-            context.duration = 0.2
-            self.animator().alphaValue = 1
+        currentAnimation?.stop()
+
+        var frame = NSRect(
+            origin: frame.origin,
+            size: CGSize(
+                width: size.width,
+                height: size.height + 52 // 52 is the title bar height
+            )
+        )
+
+        if let screenFrame = screen?.visibleFrame {
+            if frame.minX < screenFrame.minX {
+                frame.origin.x = screenFrame.minX
+            }
+
+            if frame.minY < screenFrame.minY {
+                frame.origin.y = screenFrame.minY
+            }
+
+            if frame.maxX > screenFrame.maxX {
+                frame.origin.x = screenFrame.maxX - frame.width
+            }
+
+            if frame.maxY > screenFrame.maxY {
+                frame.origin.y = screenFrame.maxY - frame.height
+            }
+        }
+
+        if animate {
+            currentAnimation = LuminareWindowAnimation(self, frame)
+            currentAnimation?.start()
+        } else {
+            setFrame(frame, display: true)
+        }
+    }
+}
+
+// MARK: - NSWindow Animation
+
+// Vustom `NSWindow` resize animation so that it can be stopped midway
+class LuminareWindowAnimation: NSAnimation {
+    let window: NSWindow
+    let targetFrame: NSRect
+
+    init(_ window: NSWindow, _ targetFrame: NSRect) {
+        self.window = window
+        self.targetFrame = targetFrame
+        super.init(duration: 0.5, animationCurve: .easeOut)
+        super.animationBlockingMode = .nonblocking // allows the window to redraw contents while animating
+    }
+
+    @available(*, unavailable)
+    required init?(coder _: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override var currentProgress: NSAnimation.Progress {
+        didSet {
+            // The last frame of this `NSAnimation` looks a little stuttery,
+            // so we multiply the progress by 1.01, and then make sure the last
+            // frame doesn't draw
+            let progress = CGFloat(currentProgress * 1.01)
+            guard progress < 1 else {
+                return
+            }
+
+            let currentFrame = NSRect(
+                x: window.frame.origin.x + (targetFrame.origin.x - window.frame.origin.x) * progress,
+                y: window.frame.origin.y + (targetFrame.origin.y - window.frame.origin.y) * progress,
+                width: window.frame.width + (targetFrame.width - window.frame.width) * progress,
+                height: window.frame.height + (targetFrame.height - window.frame.height) * progress
+            )
+
+            window.setFrame(currentFrame, display: false)
         }
     }
 }
